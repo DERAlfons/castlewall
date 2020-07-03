50d8f9dffebe48c1471c541afb44520c7775ccbb
Jascha Bakarinow <jascha-bakarinow@web.de>
2020-07-03 03:51:03 +0200

    Check for walls exactly forming one circle

--- src/app/board-test/board-test.component.ts
+++ src/app/board-test/board-test.component.ts
@@ -116,6 +116,102 @@ export class BoardTestComponent implements OnInit {
     }
   }
 
+  check_single_circle(): boolean {
+    let start_wall = { i: 0, j: 0 };
+    while (this.hboard[start_wall.i][start_wall.j] != 'wall') {
+      if (start_wall.i == 9 && start_wall.j == 8) {
+        console.log('empty board');
+        return false;
+      }
+      else if (start_wall.i == 9) {
+        start_wall.i = 0;
+        start_wall.j += 1;
+      }
+      else {
+        start_wall.i += 1;
+      }
+    }
+
+    let vcircle = [[],[],[],[],[],[],[],[],[],[]];
+    let hcircle = [[],[],[],[],[],[],[],[],[],[]];
+    let cpath = [];
+
+    hcircle[start_wall.i][start_wall.j] = true;
+    cpath.push({ i: start_wall.i, j: start_wall.j });
+    cpath.push({ i: start_wall.i, j: start_wall.j + 1 });
+
+    while (!(cpath[0].i == cpath[cpath.length - 1].i && cpath[0].j == cpath[cpath.length - 1].j)) {
+      var nwp = this.next_wall_post(cpath[cpath.length - 2], cpath[cpath.length - 1]);
+      if (!nwp) {
+        console.log(`No valid continuation: pi: ${cpath[cpath.length - 2].i}, pj: ${cpath[cpath.length - 2].j}, ci: ${cpath[cpath.length - 1].i}, cj: ${cpath[cpath.length - 1].j}`);
+        return false;
+      }
+
+      if (this.intersect(cpath, nwp)) {
+        console.log(`Found intersection: i: ${nwp.i}, j: ${nwp.j}`);
+        return false;
+      }
+
+      if (cpath[cpath.length - 1].i + 1 == nwp.i) {
+        vcircle[nwp.j][nwp.i - 1] = true;
+      }
+      else if (cpath[cpath.length - 1].j + 1 == nwp.j) {
+        hcircle[nwp.i][nwp.j - 1] = true;
+      }
+      else if (cpath[cpath.length - 1].i - 1 == nwp.i) {
+        vcircle[nwp.j][nwp.i] = true;
+      }
+      else if (cpath[cpath.length - 1].j - 1 == nwp.j) {
+        hcircle[nwp.i][nwp.j] = true;
+      }
+
+      cpath.push(nwp);
+    }
+
+    for (let i = 0; i <= 9; i++) {
+      for (let j = 0; j <= 8; j++) {
+        if (this.vboard[i][j] == 'wall' && !vcircle[i][j]) {
+          console.log('extra walls');
+          return false;
+        }
+
+        if (this.hboard[i][j] == 'wall' && !hcircle[i][j]) {
+          console.log('extra walls');
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  next_wall_post(p_post, c_post) {
+    if (c_post.i <= 8 && c_post.i >= p_post.i && this.vboard[c_post.j][c_post.i] == 'wall') {
+      return { i: c_post.i + 1, j: c_post.j };
+    }
+    else if (c_post.j <= 8 && c_post.j >= p_post.j && this.hboard[c_post.i][c_post.j] == 'wall') {
+      return { i: c_post.i, j: c_post.j + 1 };
+    }
+    else if (c_post.i >= 1 && c_post.i <= p_post.i && this.vboard[c_post.j][c_post.i - 1] == 'wall') {
+      return { i: c_post.i - 1, j: c_post.j };
+    }
+    else if (c_post.j >= 1 && c_post.j <= p_post.j && this.hboard[c_post.i][c_post.j - 1] == 'wall') {
+      return { i: c_post.i, j: c_post.j - 1 };
+    }
+
+    return null;
+  }
+
+  intersect(path, point): boolean {
+    for (let i = 1; i < path.length; i++) {
+      if (path[i].i == point.i && path[i].j == point.j) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   check_in_out(): boolean {
     for (let i = 1; i <= 8; i++) {
       for (let j = 1; j <= 8; j++) {
@@ -208,7 +304,7 @@ export class BoardTestComponent implements OnInit {
   }
 
   checkBtn(): void {
-    if (this.check_in_out()) {
+    if (this.check_single_circle()) {
       console.log("Yay");
     }
     else {
