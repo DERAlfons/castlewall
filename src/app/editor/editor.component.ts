1910889a5169c306aaedc613a62c1459404b8790
Jascha Bakarinow <jascha-bakarinow@web.de>
2020-11-24 00:21:35 +0100

    Seperate rendering class

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,8 +1,10 @@
 import { PuzzleService } from '../puzzle.service';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+
 import { EditBoard } from '../edit-board';
+import { BoardCanvas } from '../board-canvas';
 
 @Component({
   selector: 'app-editor',
@@ -25,7 +27,7 @@ export class EditorComponent implements OnInit {
   board: EditBoard = new EditBoard({ id: null, title: 'Editor', s_representation: '?', width: 10, height: 10, hints: [] });
   puzzleDownload: SafeResourceUrl;
 
-  private render_ctx: CanvasRenderingContext2D;
+  private boardCanvas: BoardCanvas;
 
   constructor(
     private sanitizer: DomSanitizer,
@@ -36,13 +38,13 @@ export class EditorComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.render_ctx = this.canvasbg.nativeElement.getContext('2d');
+    this.boardCanvas = new BoardCanvas(this.canvasbg);
 
     const id = this.route.snapshot.paramMap.get('id');
     if (id == null) {
       this.editWidth = String(this.board.width);
       this.editHeight = String(this.board.height);
-      this.render();
+      this.boardCanvas.render(this.board);
       this.canvasbg.nativeElement.addEventListener('mousedown', event => this.handleMousedown(event));
     }
     else {
@@ -51,7 +53,7 @@ export class EditorComponent implements OnInit {
         this.updatePuzzleDownload();
         this.editWidth = String(this.board.width);
         this.editHeight = String(this.board.height);
-        this.render();
+        this.boardCanvas.render(this.board);
         this.canvasbg.nativeElement.addEventListener('mousedown', event => this.handleMousedown(event));
       });
     }
@@ -63,84 +65,14 @@ export class EditorComponent implements OnInit {
 
   updateWidth(): void {
     this.board.updateWidth(+this.editWidth);
-    this.render();
+    this.boardCanvas.render(this.board);
     this.updatePuzzleDownload();
   }
 
   updateHeight(): void {
     this.board.updateHeight(+this.editHeight);
-    this.render();
+    this.boardCanvas.render(this.board);
     this.updatePuzzleDownload();
-  }
-
-  render(): void {
-    this.render_ctx.clearRect(0, 0, this.canvasbg.nativeElement.width, this.canvasbg.nativeElement.height);
-
-    for (let i = 0; i <= this.board.width; i++) {
-      this.render_ctx.beginPath();
-      this.render_ctx.strokeStyle = 'black';
-      this.render_ctx.lineWidth = 2;
-      this.render_ctx.moveTo(20 + i * 20, 20);
-      this.render_ctx.lineTo(20 + i * 20, 20 + this.board.height * 20);
-      this.render_ctx.stroke();
-      this.render_ctx.closePath();
-    }
-    for (let i = 0; i <= this.board.height; i++) {
-      this.render_ctx.beginPath();
-      this.render_ctx.strokeStyle = 'black';
-      this.render_ctx.lineWidth = 2;
-      this.render_ctx.moveTo(20, 20 + i * 20);
-      this.render_ctx.lineTo(20 + this.board.width * 20, 20 + i * 20);
-      this.render_ctx.stroke();
-      this.render_ctx.closePath();
-    }
-
-    for (let i = 0; i < this.board.height; i++) {
-      for (let j = 0; j < this.board.width; j++) {
-        if (this.board.cboard[i][j]) {
-          this.render_ctx.fillStyle = this.board.cboard[i][j].color;
-          this.render_ctx.fillRect(20 + j * 20, 20 + i * 20, 20, 20);
-          if (this.board.cboard[i][j].direction) {
-            if (this.board.cboard[i][j].color == 'black') {
-              this.render_ctx.fillStyle = 'white';
-            }
-            else {
-              this.render_ctx.fillStyle = 'black';
-            }
-            this.render_ctx.font = '10px Arial';
-            this.render_ctx.fillText(`${this.board.cboard[i][j].walls}${this.board.cboard[i][j].direction[0]}`, 25 + j * 20, 35 + i * 20);
-          }
-          this.render_ctx.strokeStyle = 'black';
-          this.render_ctx.lineWidth = 4;
-          this.render_ctx.strokeRect(20 + j * 20, 20 + i * 20, 20, 20);
-        }
-      }
-    }
-
-    for (let i = 0; i < this.board.width; i++) {
-      for (let j = 0; j < this.board.height - 1; j++) {
-        if (this.board.vboard[i][j] == 'wall') {
-          this.connect(i + 1, j + 1, i + 1, j + 2);
-        }
-      }
-    }
-    for (let i = 0; i < this.board.height; i++) {
-      for (let j = 0; j < this.board.width - 1; j++) {
-        if (this.board.hboard[i][j] == 'wall') {
-          this.connect(j + 1, i + 1, j + 2, i + 1);
-        }
-      }
-    }
-  }
-
-  connect(x1: number, y1: number, x2: number, y2: number): void {
-    this.render_ctx.beginPath();
-    this.render_ctx.strokeStyle = 'green';
-    this.render_ctx.lineWidth = 2;
-    this.render_ctx.moveTo(10 + x1 * 20, 10 + y1 * 20);
-    this.render_ctx.lineTo(10 + x2 * 20, 10 + y2 * 20);
-    this.render_ctx.stroke();
-    this.render_ctx.closePath();
   }
 
   handleMousedown(event: MouseEvent): void {
@@ -173,7 +105,7 @@ export class EditorComponent implements OnInit {
 
     this.menuActive = false;
 
-    this.render();
+    this.boardCanvas.render(this.board);
 
     this.updatePuzzleDownload();
   }
