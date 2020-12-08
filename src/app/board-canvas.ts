52b4d92992c29ff6aa6d38696347a98f3efc442e
Jascha <jascha-bakarinow@web.de>
2020-12-08 19:00:38 +0100

    Draw direction arrows instead of characters

--- src/app/board-canvas.ts
+++ src/app/board-canvas.ts
@@ -8,8 +8,15 @@ export class BoardCanvas {
   private render_ctx: CanvasRenderingContext2D;
   private cellSize: number = 40;
   private gridOffset: number = 20;
+  private arrow_black;
+  private arrow_white;
 
   constructor(canvas: ElementRef<HTMLCanvasElement>) {
+    this.arrow_black = new Image();
+    this.arrow_black.src = '../assets/arrow_black.svg';
+    this.arrow_white = new Image();
+    this.arrow_white.src = '../assets/arrow_white.svg';
+
     this.htmlCanvas = canvas;
     this.render_ctx = canvas.nativeElement.getContext('2d');
   }
@@ -42,14 +49,59 @@ export class BoardCanvas {
           this.render_ctx.fillStyle = board.cboard[i][j].color;
           this.render_ctx.fillRect(this.gridOffset + j * this.cellSize, this.gridOffset + i * this.cellSize, this.cellSize, this.cellSize);
           if (board.cboard[i][j].direction) {
+            let arrow;
             if (board.cboard[i][j].color == 'black') {
               this.render_ctx.fillStyle = 'white';
+              arrow = this.arrow_white;
             }
             else {
               this.render_ctx.fillStyle = 'black';
+              arrow = this.arrow_black;
             }
             this.render_ctx.font = `${this.cellSize / 2}px Arial`;
-            this.render_ctx.fillText(`${board.cboard[i][j].walls}${board.cboard[i][j].direction[0]}`, this.gridOffset + j * this.cellSize + this.cellSize / 4, this.gridOffset + i * this.cellSize + 3 * this.cellSize / 4);
+            let textMeasures = this.render_ctx.measureText(`${board.cboard[i][j].walls}`);
+            let textWidth = textMeasures.width;
+            let textHeight = textMeasures.actualBoundingBoxAscent;
+            let arrowWidth: number;
+            let arrowHeight: number;
+            let arrowX: number;
+            let arrowY: number;
+            let textX: number;
+            let textY: number;
+            if (board.cboard[i][j].direction == 'right') {
+              arrowWidth = this.cellSize / 2;
+              arrowHeight = this.cellSize / 4;
+              arrowX = Math.floor((this.cellSize - arrowWidth) / 2);
+              arrowY = Math.floor((this.cellSize - arrowHeight - textHeight) / 2);
+              textX = Math.floor((this.cellSize - textWidth) / 2);
+              textY = Math.floor((this.cellSize + arrowHeight + textHeight) / 2);
+              this.drawRotated(arrow, this.gridOffset + j * this.cellSize + arrowX, this.gridOffset + i * this.cellSize + arrowY, arrowWidth, arrowHeight, 0);
+            } else if (board.cboard[i][j].direction == 'left') {
+              arrowWidth = this.cellSize / 2;
+              arrowHeight = this.cellSize / 4;
+              arrowX = Math.floor((this.cellSize + arrowWidth) / 2);
+              arrowY = Math.floor((this.cellSize + arrowHeight - textHeight) / 2);
+              textX = Math.floor((this.cellSize - textWidth) / 2);
+              textY = Math.floor((this.cellSize + arrowHeight + textHeight) / 2);
+              this.drawRotated(arrow, this.gridOffset + j * this.cellSize + arrowX, this.gridOffset + i * this.cellSize + arrowY, arrowWidth, arrowHeight, Math.PI);
+            } else if (board.cboard[i][j].direction == 'up') {
+              arrowWidth = this.cellSize / 4;
+              arrowHeight = this.cellSize / 2;
+              arrowX = Math.floor((this.cellSize + textWidth - arrowWidth) / 2);
+              arrowY = Math.floor((this.cellSize + arrowHeight) / 2);
+              textX = Math.floor((this.cellSize - arrowWidth - textWidth) / 2);
+              textY = Math.floor((this.cellSize + textHeight) / 2);
+              this.drawRotated(arrow, this.gridOffset + j * this.cellSize + arrowX, this.gridOffset + i * this.cellSize + arrowY, arrowHeight, arrowWidth, -Math.PI / 2);
+            } else if (board.cboard[i][j].direction == 'down') {
+              arrowWidth = this.cellSize / 4;
+              arrowHeight = this.cellSize / 2;
+              arrowX = Math.floor((this.cellSize + textWidth + arrowWidth) / 2);
+              arrowY = Math.floor((this.cellSize - arrowHeight) / 2);
+              textX = Math.floor((this.cellSize - arrowWidth - textWidth) / 2);
+              textY = Math.floor((this.cellSize + textHeight) / 2);
+              this.drawRotated(arrow, this.gridOffset + j * this.cellSize + arrowX, this.gridOffset + i * this.cellSize + arrowY, arrowHeight, arrowWidth, Math.PI / 2);
+            }
+            this.render_ctx.fillText(`${board.cboard[i][j].walls}`, this.gridOffset + j * this.cellSize + textX, this.gridOffset + i * this.cellSize + textY);
           }
           this.render_ctx.strokeStyle = 'black';
           this.render_ctx.lineWidth = 4;
@@ -72,6 +124,14 @@ export class BoardCanvas {
         }
       }
     }
+  }
+
+  drawRotated(arrow, posX, posY, width, height, angle) {
+    this.render_ctx.save();
+    this.render_ctx.translate(posX, posY);
+    this.render_ctx.rotate(angle);
+    this.render_ctx.drawImage(arrow, 0, 0, width, height);
+    this.render_ctx.restore();
   }
 
   connect(x1: number, y1: number, x2: number, y2: number): void {
